ae194ce23b160fd5ccb6e7502ef1c960147fa785
PC-ANTONIO-CASA <antoniollorentepalomo@gmail.com>
2026-02-14 18:56:04 +0100

    por hacer método CARGAR TABLA

--- src/main/java/view/AucorsaView.java
+++ src/main/java/view/AucorsaView.java
@@ -1,26 +1,36 @@
 package view;
 
 import javax.swing.*;
-import javax.swing.border.Border;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class AucorsaView extends JFrame {
-    JPanel panelBtns = new JPanel();
-    JButton btnadd = new JButton("Añadir");
-    JButton btndelt = new JButton("Borrar");
+    private JPanel panelBtns = new JPanel();
+    private JButton btnadd = new JButton("Añadir");
+    private JButton btndelt = new JButton("Borrar");
+
+    private DefaultTableModel tablaDrivers;
+    private JTable vistaTabla;
 
     public AucorsaView() {
         super("Sistema de gestión AUCORSA");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(500,400);
         this.setLocationRelativeTo(null);
-        this.setVisible(true);
 
         setLayout(new BorderLayout());
 
         this.add(panelBtns, BorderLayout.NORTH);
         panelBtns.add(btnadd);
         panelBtns.add(btndelt);
+
+        tablaDrivers = new DefaultTableModel(new String[]{"Número", "Nombre", "Apellidos"},0);
+        vistaTabla = new JTable(tablaDrivers);
+
+        this.add(vistaTabla, BorderLayout.CENTER);
+
+        this.setVisible(true);
+
     }
 
     public JPanel getPanelBtns() {
@@ -29,6 +39,22 @@ public class AucorsaView extends JFrame {
 
     public void setPanelBtns(JPanel panelBtns) {
         this.panelBtns = panelBtns;
+    }
+
+    public DefaultTableModel getTablaDrivers() {
+        return tablaDrivers;
+    }
+
+    public void setTablaDrivers(DefaultTableModel tablaDrivers) {
+        this.tablaDrivers = tablaDrivers;
+    }
+
+    public JTable getVistaTabla() {
+        return vistaTabla;
+    }
+
+    public void setVistaTabla(JTable vistaTabla) {
+        this.vistaTabla = vistaTabla;
     }
 
     public JButton getBtnadd() {
@@ -46,4 +72,6 @@ public class AucorsaView extends JFrame {
     public void setBtndelt(JButton btndelt) {
         this.btndelt = btndelt;
     }
+
+    public void cargarTabla
 }
